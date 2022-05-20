12c371f35ef36b06aa689935b3f7f20c3c0a04f7
JuneYub <ajsjdlwj0123@gmail.com>
2022-05-20 13:08:00 +0900

    v0.0

---
+++ src/main/java/com/example/ai_interview/AiInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ai_interview/AiInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
