fdde87387ef5570b043e49f92125d7da0d41c2b0
JuneYub <ajsjdlwj0123@gmail.com>
2022-05-20 13:44:35 +0900

    v0.1

--- src/main/java/com/example/ai_interview/AiInterviewApplication.java
+++ src/main/java/com/example/ai_interview/AiInterviewApplication.java
@@ -1,9 +1,13 @@
 package com.example.ai_interview;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class AiInterviewApplication {
 
     public static void main(String[] args) {
